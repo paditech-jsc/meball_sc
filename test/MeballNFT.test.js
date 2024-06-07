336723e2f6743bbf000617a7e5adc5efe06b7e20
tuannt1 <tuannt1@paditech.com>
2024-06-07 15:40:45 +0700

    add test for contract

--- test/MeballNFT.test.js
+++ test/MeballNFT.test.js
@@ -1,13 +1,13 @@
 const { ethers, upgrades } = require("hardhat");
 const { expect } = require("chai");
 
-
-describe("CP Token", async () => {
+describe("Meball NFT", async () => {
     let owner;
     let addr1;
     let addr2;
     let meballNFTContract;
     let signature;
+    let mintFee;
 
     const createSignature = async (
         meballNFTContractAddress,
@@ -45,33 +45,78 @@ describe("CP Token", async () => {
         deadline = Math.floor(Date.now() / 1000 + 5 * 60);
         const probabilities = ["1", "8", "3", "3", "1", "3", "1", "3", "8", "2", "3", "8", "3", "2", "8", "1", "3", "8", "8", "8", "3", "3", "1", "8"];
         const ipfsLinks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
-        meballNFTContract = await (await ethers.getContractFactory("MeballNFT")).deploy(probabilities, ipfsLinks)
+        mintFee = ethers.utils.parseEther("0.01");
+        meballNFTContract = await (await ethers.getContractFactory("MeballNFT")).deploy(probabilities, ipfsLinks, mintFee)
 
     });
 
+    describe("Deploy correctly", () => {
+        it("Should deploy with correct initial values", async function () {
+            expect(await meballNFTContract.mintFee()).to.equal(mintFee);
+            expect(await meballNFTContract.nextTokenId()).to.equal(0);
+        });
+    })
+
     describe("Mint token", () => {
-        before(async () => {
-            const randomValues = ["abc", "xyz"];
+        it("Should mint NFTs correctly", async () => {
+            const randomValues = ["randomValue1", "randomValue2"];
             const hashRandomValues = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(randomValues.join('')));
-            console.log(hashRandomValues)
-            signature = await createSignature(
-                meballNFTContract.address,
-                owner,
-                owner.address,
-                hashRandomValues,
-                14
-            );
+            signature = createSignature(meballNFTContract.address, owner, addr1.address, hashRandomValues, 14)
+            const tx = await meballNFTContract.connect(addr1).mintNFTs({
+                requester: addr1.address,
+                randomValues: randomValues,
+                hashRandomValues: hashRandomValues,
+                nonce: 14
+            }, signature, {
+                value: mintFee.mul(randomValues.length)
+            });
+            const receipt = await tx.wait(1)
+            console.log(receipt.gasUsed.toString())
 
-            const tx = await meballNFTContract.mintNFTs({
-                requester: owner.address,
-                hashRandomValues,
-                nonce: 14,
-            }, signature, randomValues)
-            await tx.wait();
-
+            expect(await meballNFTContract.nextTokenId()).to.equal(randomValues.length);
+            expect(await meballNFTContract.tokenIdToTeam(0)).to.be.within(0, 23);
         });
 
-        it("Balance of requester is greater than zero", async () => {
+        it("Should reject minting with insufficient fee", async function () {
+            const randomValues = ["randomValue1", "randomValue2"];
+            const hashRandomValues = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(randomValues.join('')));
+            signature = createSignature(meballNFTContract.address, owner, addr1.address, hashRandomValues, 14)
+            await expect(
+                meballNFTContract.connect(addr1).mintNFTs({
+                    requester: addr1.address,
+                    randomValues: randomValues,
+                    hashRandomValues: hashRandomValues,
+                    nonce: 14
+                }, signature, {
+                    value: mintFee
+                })
+            ).to.be.revertedWith("Mint Fee not enough");
+        });
+
+        it("Should not allow duplicate signatures", async function () {
+            const randomValues = ["test_value_168"];
+            const hashRandomValues = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(randomValues.join('')));
+            signature = createSignature(meballNFTContract.address, owner, addr1.address, hashRandomValues, 14)
+
+            await meballNFTContract.connect(addr1).mintNFTs({
+                requester: addr1.address,
+                randomValues: randomValues,
+                hashRandomValues: hashRandomValues,
+                nonce: 14
+            }, signature, {
+                value: mintFee
+            })
+
+            await expect(
+                meballNFTContract.connect(addr1).mintNFTs({
+                    requester: addr1.address,
+                    randomValues: randomValues,
+                    hashRandomValues: hashRandomValues,
+                    nonce: 14
+                }, signature, {
+                    value: mintFee
+                })
+            ).to.be.revertedWith("Signature already used");
 
         });
 
--- test/StakeToken.test.js
+++ test/StakeToken.test.js
@@ -12,7 +12,7 @@ describe("NFT stake", async () => {
         [owner, addr1, addr2] = await ethers.getSigners();
         rewardToken = await (await ethers.getContractFactory("Meball")).deploy();
 
-        const tokenStakeFactory = await ethers.getContractFactory("TokenStake");
+        const tokenStakeFactory = await ethers.getContractFactory("MeballStake");
         tokenStakeContract = await upgrades.deployProxy(tokenStakeFactory, [rewardToken.address, rewardToken.address], { kind: "uups" })
 
         season = 1;
@@ -58,10 +58,8 @@ describe("NFT stake", async () => {
         it("Should allow users to claim rewards", async function () {
             await tokenStakeContract.createReward([addr1.address], [10], 1);
             const balanceBefore = await rewardToken.balanceOf(addr1.address);
-            console.log(balanceBefore.toString())
             await tokenStakeContract.connect(addr1).claim(1);
             const balanceAfter = await rewardToken.balanceOf(addr1.address);
-            console.log(balanceAfter.toString())
             expect(balanceAfter.sub(balanceBefore)).to.equal(ethers.utils.parseEther("10"));
         });
 
@@ -69,45 +67,5 @@ describe("NFT stake", async () => {
             await expect(tokenStakeContract.connect(addr1).claim(1)).to.be.revertedWith("NothingToClaim");
         });
     });
-    // describe("create reward", () => {
-    //     it("Set the reward successfully", async () => {
 
-    //         tx = await administrator
-    //             .connect(owner)
-    //             .submitTransaction(
-    //                 nftStakeContract.address,
-    //                 0,
-    //                 nftStakeContract.interface.encodeFunctionData("createReward", [
-    //                     [addr1.address, addr2.address], [50000, 50000], 100000, 1
-    //                 ])
-    //             );
-    //         await tx.wait(0);
-
-    //         tx = await administrator.connect(addr1).confirmTransaction(0);
-    //         await tx.wait(0);
-
-    //         tx = await administrator.connect(owner).confirmTransaction(0);
-    //         await tx.wait(0);
-
-    //         tx = await administrator.connect(owner).executeTransaction(0);
-    //         await tx.wait(0);
-    //     })
-
-    // })
-
-    // describe("claim", () => {
-    //     before(async () => {
-    //         rewardToken.connect(owner).transfer(nftStakeContract.address, ethers.utils.parseEther("100000"))
-    //     })
-    //     it(" unstake nft and claim token successfully", async () => {
-    //         await nftStakeContract.connect(addr1).claim([0, 1], 1);
-    //         expect(await nftContract.ownerOf(0)).to.equal(addr1.address)
-    //         expect(await nftContract.ownerOf(1)).to.equal(addr1.address)
-
-    //         // //other address claim success
-    //         // await nftStakeContract.connect(addr2).claim([0, 1], 1);
-    //         // expect(await nftContract.ownerOf(0)).to.equal(addr1.address)
-    //         // expect(await nftContract.ownerOf(1)).to.equal(addr1.address)
-    //     })
-    // })
 });
